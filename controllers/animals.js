c8d7f6afe58985b092ea633811d78b699b813602
Abdel7004 <abdelrahmanh15@gmail.com>
2023-04-02 17:40:15 -0400

    server.js setup

---
+++ controllers/animals.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const { fruits } = require('../models/Animals');
+const router = express.Router();
--- server.js
+++ server.js
@@ -2,15 +2,50 @@
 const express = require('express');
 const app = express();
 const PORT = 3000
+const methodOverride = require('method-override')
 
+// Controller
+const animalsController = require('./controllers/animals');
+
+// Models - Database stuff
+const models = require('./models/Animals');
+
+const animals = models.animals
+
+// Controllers - routes
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+// without urlencoded we get req.body undefined
+app.use(express.json()); //parse JSON data in the request body
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    res.send('default route')
+app.use((req,res,next) => {
+    console.log('this is my own middleware')
+    // middleware does something
+    // next tells server to do the next thing in the cycle
+    next()
 })
 
-const fruitsController = require('./controllers/animals');
-app.use('/animals', fruitsController);
+// Routes
+
+app.get('/api', (req, res) => {
+    res.json({
+        models,
+        status: 200
+    })
+})
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+})
+
+app.use('/animals', animalsController);
+
+app.get('/*', (req, res) => {
+    res.render("404.ejs")
+})
 
 // Listener
-app.listen(PORT, () => console.log(`express is listening on port: ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`express is listening on port: ${PORT}`)
+})
